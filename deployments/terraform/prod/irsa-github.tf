195b2986abd3a9e2b23b1b785c26a01dee197168
NIROOZbx <nyroozashraf@gmail.com>
2026-06-26 04:19:22 +0530

    infra: define OIDC provider for GitHub Actions in Terraform

--- deployments/terraform/prod/irsa-github.tf
+++ deployments/terraform/prod/irsa-github.tf
@@ -2,6 +2,13 @@
 # GitHub Actions CI/CD OIDC Roles
 # ==============================================================================
 
+# Create the OIDC provider for GitHub Actions
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+}
+
 # ----------------------------------------------------
 # 1. envoy-backend Deployer Role
 # ----------------------------------------------------
@@ -26,7 +33,7 @@ data "aws_iam_policy_document" "github_oidc_trust" {
     }
 
     principals {
-      identifiers = ["arn:aws:iam::711396988882:oidc-provider/token.actions.githubusercontent.com"]
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
       type        = "Federated"
     }
   }
@@ -68,7 +75,7 @@ data "aws_iam_policy_document" "billing_oidc_trust" {
     }
 
     principals {
-      identifiers = ["arn:aws:iam::711396988882:oidc-provider/token.actions.githubusercontent.com"]
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
       type        = "Federated"
     }
   }
